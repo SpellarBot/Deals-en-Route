48ee160cddc68a136d81ab3bea67bbc5e424d794
jinal <rajat@solulab.com>
2017-10-25 16:14:24 +0530

    removed unwanted code

--- app/Http/Services/MailTrait.php
+++ app/Http/Services/MailTrait.php
@@ -9,17 +9,11 @@ trait MailTrait {
     
     
   public function sendMail($array_mail){
-Mail::send(array('html' => 'emails.verify'), $array_mail['data'],
-        function($message) use ($array_mail)
-{
-    $message->to($array_mail['to']);
-});
    if($array_mail['type']=='verify'){
          Mail::to($array_mail['to'])->send(new EmailVerify($array_mail['data']));
    }else if($array_mail['type']=='password'){
           Mail::to($array_mail['to'])->send(new SendPasswordMail($array_mail['data']));
    }
-    
   }
   
   public function confirmCode(){
