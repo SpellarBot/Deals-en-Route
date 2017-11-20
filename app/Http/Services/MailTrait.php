24f58a7fdc793bc68ae6ce0a0a9142ae337159d5
Virendra Solulab <virendra@solulab.com>
2017-11-20 11:22:46 +0530

    Updated Email temp

--- app/Http/Services/MailTrait.php
+++ app/Http/Services/MailTrait.php
@@ -6,8 +6,18 @@ use Mail;
 use App\Mail\EmailVerify;
 use App\Mail\SendPasswordMail;
 use App\Mail\EmailVerifyVendor;
-use App\Mail\EmailPaymentFailed;
 use App\Mail\ContactUs;
+use App\Mail\PaymentSuccess;
+use App\Mail\EmailPaymentFailed;
+use App\Mail\PaymentCancel;
+use App\Mail\SubscriptionCancelSuccees;
+use App\Mail\SubscriptionCancelFailed;
+use App\Mail\SubscriptionUpgradeFailed;
+use App\Mail\SubscriptionUpgradeSuccess;
+use App\Mail\SubscriptionDowngradeFailed;
+use App\Mail\SubscriptionDowngradeSuccess;
+use App\Mail\AdditionFeaturePurchaseSuccess;
+use App\Mail\AdditionFeaturePurchaseFailed;
 
 trait MailTrait {
 
@@ -23,10 +33,31 @@ trait MailTrait {
             Mail::to($array_mail['to'])->send(new EmailPaymentFailed($array_mail['data']));
         } else if ($array_mail['type'] == 'contactuser') {
             Mail::to($array_mail['to'])->send(new ContactUs($array_mail['data']));
+        } else if ($array_mail['type'] == 'payment_success') {
+            Mail::to($array_mail['to'])->send(new PaymentSuccess($array_mail['data']));
+        } else if ($array_mail['type'] == 'payment_cancel') {
+            Mail::to($array_mail['to'])->send(new PaymentCancel($array_mail['data']));
+        } else if ($array_mail['type'] == 'subscription_cancel_success') {
+            Mail::to($array_mail['to'])->send(new SubscriptionCancelSuccees($array_mail['data']));
+        } else if ($array_mail['type'] == 'subscription_cancel_failed') {
+            Mail::to($array_mail['to'])->send(new SubscriptionCancelFailed($array_mail['data']));
+        } else if ($array_mail['type'] == 'subscription_upgrade_failed') {
+            Mail::to($array_mail['to'])->send(new SubscriptionUpgradeSuccess($array_mail['data']));
+        } else if ($array_mail['type'] == 'subscription_upgrade_success') {
+            Mail::to($array_mail['to'])->send(new SubscriptionUpgradeFailed($array_mail['data']));
+        } else if ($array_mail['type'] == 'subscription_downgrade_failed') {
+            Mail::to($array_mail['to'])->send(new SubscriptionDowngradeFailed($array_mail['data']));
+        } else if ($array_mail['type'] == 'subscription_downgrade_success') {
+            Mail::to($array_mail['to'])->send(new SubscriptionDowngradeSuccess($array_mail['data']));
+        } else if ($array_mail['type'] == 'additional_feature_purchase_success') {
+            Mail::to($array_mail['to'])->send(new AdditionFeaturePurchaseSuccess($array_mail['data']));
+        } else if ($array_mail['type'] == 'additional_feature_purchase_failed') {
+            Mail::to($array_mail['to'])->send(new AdditionFeaturePurchaseFailed($array_mail['data']));
         }
     }
 
-    public function confirmCode() {
+    public
+            function confirmCode() {
         
     }
 
