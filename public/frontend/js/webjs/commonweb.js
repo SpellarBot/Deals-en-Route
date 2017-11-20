3c293f9c185b4ed144f72174838e3edb45dd0048
jinal <rajat@solulab.com>
2017-11-20 18:41:15 +0530

    added the js file

--- public/frontend/js/webjs/commonweb.js
+++ public/frontend/js/webjs/commonweb.js
@@ -1,53 +1,58 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-    
-    $('#loadingDiv').hide().ajaxStart(function() {
+
+    $('#loadingDiv').hide().ajaxStart(function () {
         $('#loadingDiv').show();
-    }).ajaxStop(function() {
+    }).ajaxStop(function () {
         $('#loadingDiv').hide();
     });
 
- //modal close
+    //modal close
     $(".modal").on("hidden.bs.modal", function () {
-       $('.form-control').val('');
+        $('.form-control').val('');
         $(".form-group").removeClass('has-error');
         $(".input-group").removeClass('has-error');
         $(".help-block").html('');
         $('.alert-danger').hide();
     });
- 
+
 });
 
-var hexDigits = new Array ("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+var hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
 
 
 //clear form data
- function clearFormData(id){
- 
-        $('#'+id)[0].reset();
-        $(".form-group").removeClass('has-error');
-        $(".help-block").html('');
+function clearFormData() {
 
-                 
- }  
+    $('#create-coupon').find("input[type=text],input[type=number],input[type=hidden]").not('input[name=_token]').val("");
+    $('.wizard-inner .nav-tabs a:first').tab('show');
+    $(".wizard-inner .nav-tabs li:not(:first-child)").addClass("disabled");
+    $('.fileinput').fileinput('clear');
+    $(".form-group").removeClass('has-error');
+    $(".help-block").html('');
+    $('.stepsincrement').val(1);
+    $('.validationcheck').val(0);
+
+
+}
 
 function setToastNotification(objDataParams) {
 
     var header = "Deals en Route";
-    if(typeof objDataParams.header != 'undefined') {
+    if (typeof objDataParams.header != 'undefined') {
         header = objDataParams.header;
     }
 
     var message = "Welcome!!";
-    if(typeof objDataParams.message != 'undefined') {
+    if (typeof objDataParams.message != 'undefined') {
         message = objDataParams.message;
     }
 
-    setTimeout(function() {
+    setTimeout(function () {
         toastr.options = {
             closeButton: true,
             progressBar: true,
@@ -59,66 +64,66 @@ function setToastNotification(objDataParams) {
     }, 1000);
 }
 
-$(window).load(function() {
+$(window).load(function () {
     $("#loadingDiv").fadeOut("slow");
 });
 
 
 
 // for jquery response json notification
-function setErrorNotification(data){
-      if(data.responseJSON.status==0){
+function setErrorNotification(data) {
+    if (data.responseJSON.status == 0) {
         $('.alert-danger').show();
-        setTimeout(function() {
+        setTimeout(function () {
             $('.alert-danger').fadeOut('slow');
-            },10000);
-       $('.errormessage').html(data.responseJSON.message);
-   }else{
-         $('.alert-success').show();
-        setTimeout(function() {
-            $('.alert-success').fadeOut('slow');
-            },10000);
-       $('.successmessage').html(data.responseJSON.message);
-   }
-    
-}
-
-function setFlashSuccessNotification(){
-    
+        }, 10000);
+        $('.errormessage').html(data.responseJSON.message);
+    } else {
         $('.alert-success').show();
-        setTimeout(function() {
+        setTimeout(function () {
             $('.alert-success').fadeOut('slow');
-            },10000);
+        }, 10000);
+        $('.successmessage').html(data.responseJSON.message);
+    }
 
 }
-function setFlashErrorNotification(){
-    
-        $('.alert-danger').show();
-        setTimeout(function() {
-            $('.alert-danger').fadeOut('slow');
-            },10000);
+
+function setFlashSuccessNotification() {
+
+    $('.alert-success').show();
+    setTimeout(function () {
+        $('.alert-success').fadeOut('slow');
+    }, 10000);
+
+}
+function setFlashErrorNotification() {
+
+    $('.alert-danger').show();
+    setTimeout(function () {
+        $('.alert-danger').fadeOut('slow');
+    }, 10000);
 
 }
 
 // for dashboard notiufication
-function setDashboardNotification(response){
+function setDashboardNotification(response) {
 
-      if(response.status==0){
+    if (response.status == 0) {
         $('.alert-danger').show();
-        setTimeout(function() {
+        setTimeout(function () {
             $('.alert-danger').fadeOut('slow');
-            },10000);
-       $('.errormessage').html(response.message);
-   }else{
-         $('.alert-success').show();
-        setTimeout(function() {
+        }, 10000);
+        $('.errormessage').html(response.message);
+    } else {
+        $('.alert-success').show();
+        setTimeout(function () {
             $('.alert-success').fadeOut('slow');
-            },10000);
-       $('.successmessage').html(response.message);
-   }
-   
-   
+        }, 10000);
+        $('.successmessage').html(response.message);
+    }
 
-    
+
+
+
 }
 
