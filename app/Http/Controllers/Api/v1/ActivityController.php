70ebf66ad14e69fd5d9d446d3606d96074c2619d
Ubuntu <ubuntu@ip-172-31-36-195.us-east-2.compute.internal>
2017-12-07 13:26:22 +0000

    Changes

--- app/Http/Controllers/Api/v1/ActivityController.php
+++ app/Http/Controllers/Api/v1/ActivityController.php
@@ -54,7 +54,7 @@ class ActivityController extends Controller {
             }
             return $this->responseJson('success', \Config::get('constants.NO_RECORDS'), 200);
         } catch (\Exception $e) {
-            // throw $e;
+             throw $e;
             return $this->responseJson('error', \Config::get('constants.APP_ERROR'), 400);
         }
     }
