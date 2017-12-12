30768427a17d91ca40465e46deaa737a85d9c598
Virendra Solulab <virendra@solulab.com>
2017-12-12 15:48:31 +0530

    changes Subscribe

--- resources/views/frontend/dashboard/changesub.blade.php
+++ resources/views/frontend/dashboard/changesub.blade.php
@@ -30,111 +30,113 @@
         <div class="row p-flex">
             @if($subscription['stripe_plan'] == 'gold')
             <div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3 cardm">
-                @else
-                <div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3">
-                    @endif
-                    <p class="head">GOLD</p>
-                    <p class="price"><sup>$</sup>149</p>
-                    <p class="price-pm">PER MONTH</p>
-                    <p class="deals">~ Create and Send Coupons
-                        <br><br> ~ Geolocation (Mile Radius 5-10 miles)
-                        <br><br> ~ Geofencing (one per deal up to 20,000 Sq. Feet)
-                        <br><br> ~ Analytics ( Age + Gender + Coupon Views + Potential Customer Reach)
-                        <br><br> ~ 30 Deals/ Unlimited Coupons
-                        <br><br> ~ Sticker for Store window (Deals en Route certified)</p>
-                    <p class="head2">Additional Packages</p>
-                    <p class="deals">Geolocation - $4.99/additional mile
-                        <br><br> Geo Fencing - $4.99/20,000 square feet</p>
-                    <p class="discl">*Coupons don't roll/carry over to next month
-                        <br><br> *Commission is equal to 30% of the face value discount the coupon provides. If this is less than $1.00, DER will receive $1.00</p>
-                    @if($subscription['stripe_plan'] == 'gold')
-                    <button type="button" class="btn btn-want" value="gold">Current Package</button>
+                @elseif($subscription['stripe_plan'] == 'silver')
+                <div class="col-md-4 col-sm-6 cards cards2">
                     @else
-                    <button type="button" class="btn btn-want" value="gold">{{$name}}</button>
-                    @endif
-                </div>
-                @if($subscription['stripe_plan'] == 'silver')
-                <div class="col-md-4 col-sm-6 cards cards2 cardm">
-                    @else
-                    <div class="col-md-4 col-sm-6 cards cards2">
+                    <div class="col-md-4 col-sm-6 cards cards1">
                         @endif
-
-                        <p class="head">SILVER</p>
-                        <p class="price"><sup>$</sup>99</p>
+                        <p class="head">GOLD</p>
+                        <p class="price"><sup>$</sup>149</p>
                         <p class="price-pm">PER MONTH</p>
                         <p class="deals">~ Create and Send Coupons
-                            <br><br> ~ Geolocation (Mile Radius 1-5 Miles)
+                            <br><br> ~ Geolocation (Mile Radius 5-10 miles)
                             <br><br> ~ Geofencing (one per deal up to 20,000 Sq. Feet)
-                            <br><br> ~ Analytics ( Age + Gender)
-                            <br><br> ~ 20 Deals / Unlimited Coupons
+                            <br><br> ~ Analytics ( Age + Gender + Coupon Views + Potential Customer Reach)
+                            <br><br> ~ 30 Deals/ Unlimited Coupons
                             <br><br> ~ Sticker for Store window (Deals en Route certified)</p>
                         <p class="head2">Additional Packages</p>
                         <p class="deals">Geolocation - $4.99/additional mile
                             <br><br> Geo Fencing - $4.99/20,000 square feet</p>
                         <p class="discl">*Coupons don't roll/carry over to next month
                             <br><br> *Commission is equal to 30% of the face value discount the coupon provides. If this is less than $1.00, DER will receive $1.00</p>
-                        @if($subscription['stripe_plan'] == 'silver')
-                        <button type="button" class="btn btn-want" value="silver">Current Package</button>
+                        @if($subscription['stripe_plan'] == 'gold')
+                        <button type="button" class="btn btn-want" value="gold">Current Package</button>
                         @else
-                        <button type="button" class="btn btn-want" value="silver">{{$name}}</button>
+                        <button type="button" class="btn btn-want" value="gold">{{$name}}</button>
                         @endif
-
                     </div>
-                    @if($subscription['stripe_plan'] == 'bronze')
-                    <div class="col-md-4 col-sm-6 cards cards1 cardm">
+                    @if($subscription['stripe_plan'] == 'silver')
+                    <div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3 cardm">
                         @else
-                        <div class="col-md-4 col-sm-6 cards cards1 ">
+                        <div class="col-md-4 col-sm-6 cards cards2">
                             @endif
-                            <p class="head">BRONZE</p>
-                            <p class="price"><sup>$</sup>49</p>
+
+                            <p class="head">SILVER</p>
+                            <p class="price"><sup>$</sup>99</p>
                             <p class="price-pm">PER MONTH</p>
                             <p class="deals">~ Create and Send Coupons
-                                <br><br> ~ Geolocation (Mile Radius 0-1 Miles)
+                                <br><br> ~ Geolocation (Mile Radius 1-5 Miles)
                                 <br><br> ~ Geofencing (one per deal up to 20,000 Sq. Feet)
-                                <br><br> ~ 10 Deals / Unlimited Coupons
+                                <br><br> ~ Analytics ( Age + Gender)
+                                <br><br> ~ 20 Deals / Unlimited Coupons
                                 <br><br> ~ Sticker for Store window (Deals en Route certified)</p>
                             <p class="head2">Additional Packages</p>
                             <p class="deals">Geolocation - $4.99/additional mile
                                 <br><br> Geo Fencing - $4.99/20,000 square feet</p>
                             <p class="discl">*Coupons don't roll/carry over to next month
                                 <br><br> *Commission is equal to 30% of the face value discount the coupon provides. If this is less than $1.00, DER will receive $1.00</p>
-                            @if($subscription['stripe_plan'] == 'bronze')
-                            <button type="button" class="btn btn-want btn-want1" value="bronze">Current Package</button>
+                            @if($subscription['stripe_plan'] == 'silver')
+                            <button type="button" class="btn btn-want" value="silver">Current Package</button>
                             @else
-                            <button type="button" class="btn btn-want" value="bronze">{{$name}}</button>
+                            <button type="button" class="btn btn-want" value="silver">{{$name}}</button>
                             @endif
+
+                        </div>
+                        @if($subscription['stripe_plan'] == 'bronze')
+                        <div class="col-md-4 col-md-offset-0 col-sm-6 col-sm-offset-3 cardm">
+                            @else
+                            <div class="col-md-4 col-sm-6 cards cards1">
+                                @endif
+                                <p class="head">BRONZE</p>
+                                <p class="price"><sup>$</sup>49</p>
+                                <p class="price-pm">PER MONTH</p>
+                                <p class="deals">~ Create and Send Coupons
+                                    <br><br> ~ Geolocation (Mile Radius 0-1 Miles)
+                                    <br><br> ~ Geofencing (one per deal up to 20,000 Sq. Feet)
+                                    <br><br> ~ 10 Deals / Unlimited Coupons
+                                    <br><br> ~ Sticker for Store window (Deals en Route certified)</p>
+                                <p class="head2">Additional Packages</p>
+                                <p class="deals">Geolocation - $4.99/additional mile
+                                    <br><br> Geo Fencing - $4.99/20,000 square feet</p>
+                                <p class="discl">*Coupons don't roll/carry over to next month
+                                    <br><br> *Commission is equal to 30% of the face value discount the coupon provides. If this is less than $1.00, DER will receive $1.00</p>
+                                @if($subscription['stripe_plan'] == 'bronze')
+                                <button type="button" class="btn btn-want btn-want1" value="bronze">Current Package</button>
+                                @else
+                                <button type="button" class="btn btn-want" value="bronze">{{$name}}</button>
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-                </section>
+                    </section>
 
-                @endsection
-                @section('scripts')
-                <script type="text/javascript">
-                    $('.btn-want').on('click', function (e) {
-                        e.preventDefault();
+                    @endsection
+                    @section('scripts')
+                    <script type="text/javascript">
+                        $('.btn-want').on('click', function (e) {
+                            e.preventDefault();
 
-                        $.ajax({
-                            url: $('#hidAbsUrl').val() + "/register/subcription",
-                            type: 'POST',
-                            data: {'plan_id': $(this).val(), 'user_id': "<?php echo $user_id ?>"},
-                            success: function (data) {
-                                location.reload();
-                            },
-                            beforeSend: function () {
-                                $('#loadingDiv').show();
-                            },
-                            complete: function () {
-                                $('#loadingDiv').hide();
-                            },
-                            error: function (data) {
-                                window.location.href = 'http://dealsenroute.com/dealenroute';
-                            },
+                            $.ajax({
+                                url: $('#hidAbsUrl').val() + "/register/subcription",
+                                type: 'POST',
+                                data: {'plan_id': $(this).val(), 'user_id': "<?php echo $user_id ?>"},
+                                success: function (data) {
+                                    location.reload();
+                                },
+                                beforeSend: function () {
+                                    $('#loadingDiv').show();
+                                },
+                                complete: function () {
+                                    $('#loadingDiv').hide();
+                                },
+                                error: function (data) {
+                                    window.location.href = 'http://dealsenroute.com/dealenroute';
+                                },
+
+                            });
 
                         });
+                    </script>   
 
-                    });
-                </script>   
-
-                @endsection
+                    @endsection
 
