3b589c389ee12bdd63bca8968d87602392f19226
Virendra Solulab <virendra@solulab.com>
2017-12-12 15:17:27 +0530

    Changes Sub

--- resources/views/frontend/layouts/dashboard.blade.php
+++ resources/views/frontend/layouts/dashboard.blade.php
@@ -126,7 +126,7 @@
         <script type="text/javascript" src="{{ asset('frontend/js/webjs/settings.js') }}"></script>
         <script type="text/javascript" src="{{ asset('frontend/js/webjs/couponlist.js') }}"></script>
         <script type="text/javascript" src="{{ asset('frontend/js/custom.js') }}"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key={{ \Config::get('googlemaps.key') }}&libraries=drawing&libraries=places&callback=Maps&callback=initAutocomplete"
+        <script src="https://maps.googleapis.com/maps/api/js?key={{ \Config::get('googlemaps.key') }}&libraries=drawing&libraries=places&callback=initCallback"
         async defer></script>
 
         @yield('scripts')
