70f025d3968d82a0df5c9e8447b0647fb405ed7c
jinal <rajat@solulab.com>
2017-12-19 14:30:12 +0530

    added the pages

---
+++ resources/views/frontend/refund.blade.php
@@ -0,0 +1,36 @@
+@extends('frontend.layouts.login')
+@section('title', 'Deals en Route|Refund Policy')
+@section('content')
+
+<header class="staticpage">
+    <div class="container">
+        <div class="header">
+            <div class="row">
+                <div class="col-sm-6">
+                    <a href="index.html" class="logo"> <img src="<?php echo \Config::get('app.url') . '/public/frontend/img/logo.png' ?>"></a>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <h2>Refund Policy</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="staticmain">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec euismod arcu. Nullam rutrum neque sed ornare vestibulum. Proin hendrerit libero quis turpis commodo consequat. Pellentesque posuere et felis vitae lobortis. Sed pretium tortor convallis pharetra lobortis. Quisque lacus magna, semper id nibh gravida, venenatis venenatis mauris. In eget dui diam. Aliquam eu porta metus. Pellentesque venenatis imperdiet mollis. Aliquam congue ornare tincidunt. Proin et purus leo. Mauris ut lobortis ligula, sit amet facilisis velit. Nunc lobortis vehicula tortor, ut ornare sapien pharetra vel.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
