bcabb1a2dbacac7a0c1d9cb7105bc8cf127da29f
Virendra Solulab <virendra@solulab.com>
2018-02-06 18:34:11 +0530

    Changes

--- app/Http/Controllers/Api/v2/VendorController.php
+++ app/Http/Controllers/Api/v2/VendorController.php
@@ -79,7 +79,8 @@ class VendorController extends Controller {
                 array_push($data['vendor_details']['hours_of_operations'], $dataVendorHour);
             }
 
-            return $this->responseJson('success', \Config::get('constants.VENDOR_RATINGDETAILS'), 200, $data);
+//            return $this->responseJson('success', \Config::get('constants.VENDOR_RATINGDETAILS'), 200, $data);
+            return $this->responseJson('success', 'Vendor Rating Submited Successfully', 200, $data);
         } catch (\Exception $e) {
             //throw $e;
             return $this->responseJson('error', \Config::get('constants.APP_ERROR'), 400);
