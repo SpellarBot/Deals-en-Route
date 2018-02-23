4cf50d3413d56a980d6a9341eb5e18b94696709f
Virendra Solulab <virendra@solulab.com>
2018-02-23 14:43:08 +0530

    Hours of operation

--- resources/views/frontend/layouts/dashboard.blade.php
+++ resources/views/frontend/layouts/dashboard.blade.php
@@ -30,7 +30,6 @@
         <div class="wrapper">
 
             <div class="errorpopup">
-                {{url()->current()}}
                 @if($hoursmsg == true)
                 <div class="alert alert-danger alert-dismissible" role="alert">
                     <button type="button" class="close hoursofoperationalert" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
