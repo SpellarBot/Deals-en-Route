8b446f3ffefc5a66a0f8b7d47c7376b4c8860e00
Kamlesh Savaj <kamlesh@solulab.com>
2018-03-19 13:22:00 +0530

    New home page design changes.

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -183,7 +183,22 @@
     </div>
 
     <div class="search-footer-bg">
-    	
+    	<footer class="footer-content">
+            <ul class="social">
+                <li class="facebook"> <a target="_blank" href="https://www.facebook.com/dealsenroute"><i class="fa fa-facebook"></i> <span>Facebook</span> </a> </li>
+                <li class="linkedin"> <a target="_blank" href="https://www.linkedin.com/company/11119147"><i class="fa fa-linkedin"></i> <span>LinkedIn</span> </a> </li>
+                <li class="twitter"> <a target="_blank" href="https://twitter.com/dealsenroute"><i class="fa fa-twitter"></i> <span>Twitter</span> </a> </li>
+                <li class="instagram"> <a target="_blank" href="https://www.instagram.com/dealsenroute"><i class="fa fa-instagram"></i> <span>Instagram</span> </a> </li>
+            </ul>
+            <div class="links">
+                <ul>
+                    <li class="tos"> <a target="_blank" href="http://dealsenroute.com/terms_condition">Terms and Conditions</a> </li>
+                    <li class="privacy"> <a target="_blank" href="http://dealsenroute.com/privacy_policy">Privacy Policy</a> </li>
+                    <li class="help"> <a target="_blank" href="http://dealsenroute.com/help">Help</a> </li>
+                </ul>
+            </div>
+            <p class="copyright"> Copyright © <script>document.write(new Date().getFullYear())</script>2018 <a href="#">Deals en Route</a>. All Rights Reserved.</p>
+    	</footer>
     </div>
 </div>
 <!-- end of search -->
